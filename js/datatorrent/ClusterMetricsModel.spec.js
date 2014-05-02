513d938b289b1418df88c173c6d347a7e46aed68
Andy Perlitch <andyperlitch@gmail.com>
2014-05-02 15:46:55 -0700

    UPDATED YEAR IN LICENSE HEADERS

--- js/datatorrent/ClusterMetricsModel.spec.js
+++ js/datatorrent/ClusterMetricsModel.spec.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */var M = require('./ClusterMetricsModel');
+ */
+
+var M = require('./ClusterMetricsModel');
 describe('ClusterMetricsModel.js', function() {
     
 	var sandbox, m;
