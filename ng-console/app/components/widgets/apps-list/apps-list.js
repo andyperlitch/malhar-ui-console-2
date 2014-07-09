36feeb0809f30295b2472adebf6b47f8fce9289b
Andy Perlitch <andyperlitch@gmail.com>
2014-07-08 19:52:42 -0700

    basic appslist working

--- ng-console/app/components/widgets/apps-list/apps-list.js
+++ ng-console/app/components/widgets/apps-list/apps-list.js
@@ -17,7 +17,10 @@
 
 angular.module('dtConsole.widgets.AppsList', [
   'dtConsole.widgets.Base',
-  // 'datatorrent.mlhrTable',
+  'dtConsole.settings',
+  'dtConsole.byteFilter',
+  'dtConsole.textService',
+  'datatorrent.mlhrTable',
   'dtConsole.resources.ApplicationCollection'
 ])
 
@@ -33,15 +36,81 @@ angular.module('dtConsole.widgets.AppsList', [
   return AppsListWidget;
 })
 
-.factory('AppsListDataModel', function(BaseDataModel, ApplicationCollection) {
+.factory('AppsListDataModel', function(BaseDataModel, ApplicationCollection, settings, DtText, $filter) {
+
+  function stateFormatter(value,row) {
+    if (!value) {
+      return '-';
+    }
+    var finalStatus = row.finalStatus;
+    var html = '<span class="status-' + value.replace(' ','-').toLowerCase() + '">' + value + '</span>';
+    if ( typeof finalStatus === 'string' && finalStatus.toLowerCase() !== 'undefined' ) {
+      html += ' <small class="final-status" title="Final Status">(' + finalStatus + ')</small>';
+    }
+    return html;
+  }
+  stateFormatter.trustAsHtml = true;
+
+  function stateSorter(row1,row2) {
+    var state1 = settings.statusOrder.indexOf(row1.state);
+    var state2 = settings.statusOrder.indexOf(row2.state);
+    return state1 - state2;
+  }
+
+  function lifetimeFormatter(value, row) {
+    var finishedTime = row.finishedTime * 1 || +new Date() ;
+    var startedTime = row.startedTime * 1 ;
+    return $filter('timeSince')({
+      timeChunk: finishedTime - startedTime,
+      unixUptime: true,
+      max_levels: 3
+    });
+  }
+
+  function memoryFormatter(value, row) {
+    if (!value) {
+      return '-';
+    }
+    return $filter('byte')(value, 'mb');
+  }
+
+  function memorySorter(row1, row2) {
+    var v1 = row1.allocatedMB;
+    var v2 = row2.allocatedMB;
+    if (!v1 && !v2) {
+      return 0;
+    }
+    if (!v1) {
+      return -1;
+    }
+    if (!v2) {
+      return 1;
+    }
+    return v1 - v2;
+  }
+
+  var columns = [
+      { id: 'selector', key: 'id', label: '', format: 'selector', width: '40px', lock_width: true },
+      { id: 'id', label: DtText.get('id_label'), key: 'id', sort: 'string', filter: 'like', format: $filter('appIdLink'), trustFormat: true },
+      { id: 'name', key: 'name', label: DtText.get('name_label'), sort: 'string', filter: 'like' },
+      { id: 'state', label: DtText.get('state_label'), key: 'state', format: stateFormatter, sort: stateSorter, filter:'like' },
+      { id: 'user', key: 'user', label: DtText.get('user_label'), sort: 'string', filter:'like' },
+      { id: 'startedTime', label: DtText.get('started_label'), key: 'startedTime', sort: 'number', filter: 'date', format: $filter('timeSince') },
+      { id: 'lifetime', label: DtText.get('lifetime_label'), key: 'startedTime', filter: 'date', format: lifetimeFormatter  },
+      { id: 'allocatedMB', label: DtText.get('memory_label'), key: 'allocatedMB', sort: memorySorter, filter: 'number', format: memoryFormatter }
+  ];
 
   var AppsListDataModel = BaseDataModel.extend({
 
     init: function() {
+      
+      this.widgetScope.columns = columns;
+      this.widgetScope.selected = [];
+
       this.resource = new ApplicationCollection();
       this.resource.fetch();
       this.resource.subscribe(this.widgetScope);
-      this.widgetScope.data = this.resource.data;
+      this.widgetScope.resource = this.resource;
     },
 
     destroy: function() {
