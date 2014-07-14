34bc19caf1833dc38e3ad60fc0a399992c76d630
Andy Perlitch <andyperlitch@gmail.com>
2014-07-14 10:40:24 -0700

    removed commented out code, added exception override

--- ng-console/app/app.js
+++ ng-console/app/app.js
@@ -50,59 +50,9 @@ angular.module('app', [
       redirectTo: '/ops'
     });
 });
-
-
-
-
-
-
-
-// $routeProvider
-
-//   // CONFIGURATION
-//   .when('/config', {
-//     templateUrl: 'views/config/index.html',
-//     label: 'Configuration'
-//   })
-//   .when('/config/welcome', {
-//     templateUrl: 'views/config/wizard.html',
-//     label: 'Installation Wizard'
-//   })
-//   .when('/config/license-info', {
-//     templateUrl: 'views/config/license.html',
-//     label: 'License Information'
-//   })
-
-//   // DEVELOPMENT
-//   .when('/dev', {
-//     templateUrl: 'views/dev/index.html',
-//     label: 'Development'
-//   })
-//   .when('/dev/build-an-etl-app', {
-//     controller: 'BuildEtlCtrl',
-//     templateUrl: 'views/dev/etl/etl.html',
-//     label: 'Build an ETL App'
-//   })
-//   .when('/dev/analyze-apache-log', {
-//     controller: 'ApacheLogMainCtrl',
-//     templateUrl: 'views/apache-log/apache-log.html',
-//     label: 'Analyze an Apache Log'
-//   })
-  
-//   // OPERATIONS
-//   .when('/ops', {
-//     controller: 'OpsCtrl',
-//     templateUrl: 'views/ops/index.html',
-//     label: 'Operations'
-//   })
-//   .when('/ops/apps/:appId', {
-//     controller: 'ApplicationCtrl',
-//     templateUrl: 'views/ops/index.html',
-//     label: 'Applications'
-//   })
-
-//   // DEFAULT
-//   .otherwise({
-//     redirectTo: '/config'
-//   });
-
+angular.module('exceptionOverride', []).factory('$exceptionHandler', function () {
+  return function (exception, cause) {
+    exception.message += ' (caused by "' + cause + '")';
+    throw exception;
+  };
+});
