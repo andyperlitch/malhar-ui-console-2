4215f33755c492e1fa605c1abd2781a9ea1f601b
Andy Perlitch <andyperlitch@gmail.com>
2014-07-15 16:26:40 -0700

    renamed all files to camelCase

---
+++ ng-console/app/components/services/confirm/confirm.js
@@ -0,0 +1,44 @@
+/*
+* Copyright (c) 2013 DataTorrent, Inc. ALL Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+'use strict';
+
+angular.module('app.components.services.confirm', ['ui.bootstrap.modal'])
+.factory('confirm', function($modal) {
+
+  function confirm(params) {
+    // Open the modal
+    return $modal.open({
+      // Use the confirm controller and template
+      controller: 'ConfirmServiceController',
+      templateUrl: 'components/services/confirm/confirm.html',
+      resolve: {
+        // inject params into template
+        params: function() {
+          return params;
+        }
+      }
+    })
+
+    // Returns a promise
+    .result;
+  }
+
+  return confirm;
+
+})
+.controller('ConfirmServiceController', function($scope, params) {
+  $scope.params = params;
+});
---
+++ ng-console/app/components/widgets/Base/Base.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2014 DataTorrent, Inc. ALL Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+angular.module('app.components.widgets.Base', [
+  'underscore',
+  'app.components.services.extend',
+  'ui.dashboard'
+])
+.factory('BaseDataModel', function(_, WidgetDataModel, extend) {
+  var BaseDataModel = extend.call(WidgetDataModel, {}, { extend: extend });
+  return BaseDataModel;
+})
+.factory('BaseWidget', function(_, extend) {
+
+  function BaseWidget(attrs) {
+    
+    // Set defaults
+    _.defaults(attrs, this.defaults);
+
+    // Extend this object
+    _.extend(this, attrs);
+
+  }
+
+  BaseWidget.prototype = {
+
+    defaults: {
+      style: {
+        width: '100%'
+      }
+    }
+
+  };
+
+  BaseWidget.extend = function(protoProps) {
+    if (protoProps.hasOwnProperty('defaults') && typeof protoProps.defaults === 'object') {
+      _.defaults(protoProps.defaults, BaseWidget.prototype.defaults);
+    }
+    return extend.call(this, protoProps);
+  };
+
+  return BaseWidget;
+
+});
