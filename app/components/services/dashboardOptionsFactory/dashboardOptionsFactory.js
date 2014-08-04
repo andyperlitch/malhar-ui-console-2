e18019cfdf151e6eeba952c02e0f5e3aebb4c6bf
Andy Perlitch <andy@datatorrent.com>
2014-08-04 13:07:53 -0700

    updated dashboardOptionsFactory

--- app/components/services/dashboardOptionsFactory/dashboardOptionsFactory.js
+++ app/components/services/dashboardOptionsFactory/dashboardOptionsFactory.js
@@ -31,7 +31,7 @@ angular.module('app.components.services.dashboardOptionsFactory', [
         onSettingsClose: defaultOnSettingsClose,
         widgetButtons: true
       };
-      return angular.extend(this, angular.copy(defaults), o);
+      return angular.extend({}, defaults, o);
     };
     return dashboardOptionsFactory;
   }
