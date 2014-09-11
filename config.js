d9be2d857063309ddeb7d49f88d1b30ce6c632fb
Nick Holub <nikholub@gmail.com>
2014-09-11 16:18:21 -0700

    Kafka Debug UI

--- config.js
+++ config.js
@@ -4,7 +4,16 @@ config.gateway = {};
 
 config.web.host = 'localhost';
 config.web.port = process.env.PORT || 9000;
+config.web.useDist = process.env.USE_DIST || false;
 config.gateway.host = 'localhost';
 config.gateway.port = process.env.GATEWAY_PORT || 3390;
+
+config.kafka = {};
+config.kafka.zookeeper = process.env.ZOOKEEPER || 'localhost:2181';
+config.kafka.lruCacheMax = 1000;
+config.kafka.enableMockData = process.env.MOCK_DATA || false;
+config.kafka.topic = {};
+config.kafka.topic.in = process.env.KAFKA_TOPIC_IN || 'test';
+config.kafka.topic.out = process.env.KAFKA_TOPIC_OUT || 'test';
 
 module.exports = config
