57b6e9b7223afc3953b5ab3182cb71bcea9060eb
Andy Perlitch <andy@datatorrent.com>
2014-09-21 23:19:21 -0700

    move function declarations out of link function

--- app/pages/dev/packages/package/dagEditor/directives/dagCanvas/dagCanvas.js
+++ app/pages/dev/packages/package/dagEditor/directives/dagCanvas/dagCanvas.js
@@ -17,7 +17,8 @@
 
 angular.module('app.pages.dev.packages.package.dagEditor.directives.dagCanvas', [
   'app.components.services.jsPlumb',
-  'app.settings'
+  'app.settings',
+  'monospaced.mousewheel'
 ])
 
 // Directive: DAG editor canvas
@@ -46,6 +47,102 @@ angular.module('app.pages.dev.packages.package.dagEditor.directives.dagCanvas', 
     });
   }
 
+  function generateNewName(key, collection, defaultName) {
+    var i = 1;
+    var name = defaultName + ' ' + i;
+    var existing;
+
+    var finder = function(o) {
+      return o[key] === name;
+    };
+
+    do {
+      existing = _.find(collection, finder);
+      if (existing) {
+        i++;
+        name = name.replace(/\s+\d+$/, '') + ' ' + i;
+      }
+    } while (existing);
+
+    return name;
+  }
+
+  /**
+   * helper function to get the operatorClass 
+   * that got dropped onto the canvas.
+   * 
+   * @param  {String} name The className of the operator
+   * @return {Object}      The class object
+   */
+  function getClass(name, scope) {
+    return _.find(scope.operatorClasses, function(c) {
+      return c.name === name;
+    });
+  }
+
+  /**
+   * Adds an operator to the application.
+   *  
+   * @param {Object} opClass Operator Class object
+   * @param {Number} x       Position to put the operator
+   * @param {Number} y       Position to put the operator
+   */
+  function addOperator(opClass, x, y, scope) {
+    var operator = {
+      name: generateNewName('name', scope.app.operators, settings.dagEditor.DEFAULT_OPERATOR_NAME),
+      opClass: opClass,
+      x: Math.max(0, x),
+      y: Math.max(0, y),
+      properties: {},
+      inputPorts: copyPorts(opClass.inputPorts, 'input'),
+      outputPorts: copyPorts(opClass.outputPorts, 'output')
+    };
+    scope.app.operators.push(operator);
+    scope.$emit('selectEntity', 'operator', operator);
+  }
+
+  function addStream(sourceOperator, sourcePort, sinkOperator, sinkPort, sinkConnection, scope) {
+    // Check for existing stream
+    var stream = _.find(scope.app.streams, function(s) {
+      return s.source.operator === sourceOperator && s.source.port === sourcePort;
+    });
+
+    if (!stream) {
+      // No existing
+      stream = {
+        name: generateNewName('name', scope.app.streams, settings.dagEditor.DEFAULT_STREAM_NAME),
+        source: {
+          operator: sourceOperator,
+          port: sourcePort
+        },
+        sinks: [
+        ]
+      };
+      // push the stream into the app scope
+      scope.app.streams.push(stream);
+    }
+
+    // Stream exists, check for sink
+    var sink = _.find(stream.sinks, function(k) {
+      return k.operator === sinkOperator && k.port === sinkPort;
+    });
+
+    // Add sink if one doesn't exist
+    if (!sink) {
+      stream.sinks.push({
+        operator: sinkOperator,
+        port: sinkPort,
+        connection_id: sinkConnection.id
+      });
+
+      angularizeSinkConnection(sinkConnection, stream, scope);
+    }
+
+    // Select the stream
+    scope.$emit('selectEntity', 'stream', stream);
+    return stream;
+  }
+
   return {
     restrict: 'A',
     templateUrl: 'pages/dev/packages/package/dagEditor/directives/dagCanvas/dagCanvas.html',
@@ -55,102 +152,6 @@ angular.module('app.pages.dev.packages.package.dagEditor.directives.dagCanvas', 
       selected: '='
     },
     link: function(scope, element){
-
-      function generateNewName(key, collection, defaultName) {
-        var i = 1;
-        var name = defaultName + ' ' + i;
-        var existing;
-
-        var finder = function(o) {
-          return o[key] === name;
-        };
-
-        do {
-          existing = _.find(collection, finder);
-          if (existing) {
-            i++;
-            name = name.replace(/\s+\d+$/, '') + ' ' + i;
-          }
-        } while (existing);
-
-        return name;
-      }
-
-      /**
-       * helper function to get the operatorClass 
-       * that got dropped onto the canvas.
-       * 
-       * @param  {String} name The className of the operator
-       * @return {Object}      The class object
-       */
-      function getClass(name) {
-        return _.find(scope.operatorClasses, function(c) {
-          return c.name === name;
-        });
-      }
-
-      /**
-       * Adds an operator to the application.
-       *  
-       * @param {Object} opClass Operator Class object
-       * @param {Number} x       Position to put the operator
-       * @param {Number} y       Position to put the operator
-       */
-      function addOperator(opClass, x, y) {
-        var operator = {
-          name: generateNewName('name', scope.app.operators, settings.dagEditor.DEFAULT_OPERATOR_NAME),
-          opClass: opClass,
-          x: Math.max(0, x),
-          y: Math.max(0, y),
-          properties: {},
-          inputPorts: copyPorts(opClass.inputPorts, 'input'),
-          outputPorts: copyPorts(opClass.outputPorts, 'output')
-        };
-        scope.app.operators.push(operator);
-        scope.$emit('selectEntity', 'operator', operator);
-      }
-
-      function addStream(sourceOperator, sourcePort, sinkOperator, sinkPort, sinkConnection) {
-        // Check for existing stream
-        var stream = _.find(scope.app.streams, function(s) {
-          return s.source.operator === sourceOperator && s.source.port === sourcePort;
-        });
-
-        if (!stream) {
-          // No existing
-          stream = {
-            name: generateNewName('name', scope.app.streams, settings.dagEditor.DEFAULT_STREAM_NAME),
-            source: {
-              operator: sourceOperator,
-              port: sourcePort
-            },
-            sinks: [
-            ]
-          };
-          // push the stream into the app scope
-          scope.app.streams.push(stream);
-        }
-
-        // Stream exists, check for sink
-        var sink = _.find(stream.sinks, function(k) {
-          return k.operator === sinkOperator && k.port === sinkPort;
-        });
-
-        // Add sink if one doesn't exist
-        if (!sink) {
-          stream.sinks.push({
-            operator: sinkOperator,
-            port: sinkPort,
-            connection_id: sinkConnection.id
-          });
-
-          angularizeSinkConnection(sinkConnection, stream, scope);
-        }
-
-        // Select the stream
-        scope.$emit('selectEntity', 'stream', stream);
-        return stream;
-      }
 
       /**
        * Listeners for connections
@@ -162,7 +163,7 @@ angular.module('app.pages.dev.packages.package.dagEditor.directives.dagCanvas', 
         var sinkOperator = info.targetEndpoint.operator;
         var sinkPort = info.targetEndpoint.port;
 
-        addStream(sourceOperator, sourcePort, sinkOperator, sinkPort, info.connection);
+        addStream(sourceOperator, sourcePort, sinkOperator, sinkPort, info.connection, scope);
       });
 
       $jsPlumb.bind('connectionDetached', function(info, originalEvent) {
@@ -192,14 +193,14 @@ angular.module('app.pages.dev.packages.package.dagEditor.directives.dagCanvas', 
 
           // if dragged item has class menu-item and dropped div has class drop-container, add module 
           if (dragEl.hasClass('operator-class') && dropEl.hasClass('dag-canvas')) {
-            var opClass = getClass(dragEl.data('classname'));
+            var opClass = getClass(dragEl.data('classname'), scope);
             $log.info('Operator class dropped on DAG canvas: ', opClass);
             var droppedOffset = ui.offset;
             var droppableOffset = dropEl.offset();
             var x = droppedOffset.left - droppableOffset.left;
             var y = droppedOffset.top - droppableOffset.top;
 
-            addOperator(opClass, x, y);
+            addOperator(opClass, x, y, scope);
           }
 
           scope.$apply();
