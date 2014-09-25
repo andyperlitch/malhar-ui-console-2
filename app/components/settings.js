c9eb9f6cc4ab30370ead13c9a76dfd50ab30addc
Andy Perlitch <andy@datatorrent.com>
2014-09-25 14:54:40 -0700

    MLHR-1423 #resolve

--- app/components/settings.js
+++ app/components/settings.js
@@ -50,6 +50,7 @@ angular.module('app.settings', [])
     maxAlertActions: 3,
     statusOrder: ['SUBMITTED','ACCEPTED','RUNNING','FAILED','FINISHED','KILLED'],
     NONENDED_APP_STATES: ['SUBMITTED','ACCEPTED','RUNNING'],
+    STARTING_APP_STATES: ['SUBMITTED', 'ACCEPTED'],
     NONENDED_CONTAINER_STATES: ['ACTIVE', 'ALLOCATED'],
     STORAGE_KEY: 'datatorrent-ui-console',
     VISIBILITY_TIMEOUT: 20000,
