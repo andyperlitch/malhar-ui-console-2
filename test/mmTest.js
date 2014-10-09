c69e7024fb6a20717e77a59a388537ee76079d10
Andy Perlitch <andy@datatorrent.com>
2014-10-09 16:05:05 -0700

    remove minimatch

--- test/mmTest.js
+++
@@ -1,5 +0,0 @@
-var Minimatch = require('minimatch').Minimatch;
-var mm = new Minimatch('app/components/**/!(*_test)+(.js)');
-
-console.log(mm.match('app/components/services/my-service.js'), 'should match non-test files');
-console.log(!mm.match('app/components/services/my-service_test.js'), 'should NOT match test files');
