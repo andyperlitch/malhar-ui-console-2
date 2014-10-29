5805e5348e0cd2a0f3486b3538dfa2b95dc823e1
Andy Perlitch <andy@datatorrent.com>
2014-10-29 01:07:14 -0700

    updated tableOptionsFactory to allow for non-widget tables

--- app/components/services/tableOptionsFactory/tableOptionsFactory_test.js
+++ app/components/services/tableOptionsFactory/tableOptionsFactory_test.js
@@ -18,15 +18,18 @@
 
 describe('Service: tableOptionsFactory', function () {
 
+  var userStorage;
+
   // load the service's module
   beforeEach(module('app.components.services.tableOptionsFactory', function($provide){
-    $provide.value('userStorage', {});
+    $provide.value('userStorage', userStorage = {});
   }));
 
-  var scope;
+  var scope, widget;
 
   beforeEach(inject(function($rootScope) {
     scope = $rootScope.$new();
+    widget = {};
   }));
 
   // instantiate service
@@ -39,14 +42,54 @@ describe('Service: tableOptionsFactory', function () {
     expect(typeof tableOptionsFactory).toEqual('function');
   });
 
-  it('should extend the passed object with storage', function() {
+  it('should extend the passed object with storage:userStorage if a widget is not provided', function() {
     var result = tableOptionsFactory({
       row_limit: 23,
       arbitrary: true
     }, scope);
     expect(result.storage).toBeDefined();
+    expect(result.storage).toEqual(userStorage);
     expect(result.arbitrary).toEqual(true);
     expect(result.row_limit).toEqual(23);
+  });
+
+  describe('storage when a widget is provided', function() {
+    
+    var dmo, storage;
+
+    beforeEach(function() {
+      var result = tableOptionsFactory({}, widget, scope);
+      dmo = widget.dataModelOptions;
+      storage = result.storage;
+    });
+
+    it('should create a dataModelOptions object if there is not one already', function() {
+      expect(widget.dataModelOptions).toBeDefined();
+    });
+
+    it('should use the dataModelOptions provided if they exist', function() {
+      var previousDmo = { foo: 'bar' };
+      var widget2 = { dataModelOptions: previousDmo };
+      tableOptionsFactory({}, widget2, scope);
+      expect(widget2.dataModelOptions === previousDmo).toEqual(true);
+    });
+
+    it('should set items on the dataModelOptions', function() {
+      storage.setItem('testing123', 'foobar');
+      expect(dmo.testing123).toBe('foobar');
+    });
+
+    it('should get items on the dataModelOptions', function() {
+      dmo.foo = 'a test';
+      expect(storage.getItem('foo')).toBe('a test');
+    });
+
+    it('should remove items from dataModelOptions', function() {
+      dmo.foo = 'bar';
+      storage.removeItem('foo');
+      expect(dmo.foo).toBeUndefined();
+    });
+
   });
 
 });
