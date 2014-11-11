11ffa4f10f3b8627df7af864ffd0b24ba098b3d0
Andy Perlitch <andy@datatorrent.com>
2014-11-11 09:24:41 -0800

    add login page module, route, and index entries

--- app/app.js
+++ app/app.js
@@ -52,6 +52,7 @@ angular.module('app', [
   'app.pages.config.installWizard',
   'app.pages.config.licenseInfo',
   'app.pages.config.systemDiagnostics',
+  'app.pages.config.login',
 
   'app.pages.ops',
   'app.pages.ops.appInstance',
